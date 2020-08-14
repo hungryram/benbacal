a5e51734fcf8f1f26d05e671b24608ecfb0866ec
Ram Dettmer <ramdettmer751@gmail.com>
2020-08-14 16:12:06 -0700

    updated site

--- static/js/main.js
+++ static/js/main.js
@@ -34,4 +34,29 @@ $('.responsive').slick({
       // instead of a settings object
     ]
   });
-              
+
+  $('.center').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 4,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
